d66b6071e8854bda8ef695b32c116dc736639485
afuzzyllama <afuzzyllama@users.noreply.github.com>
2016-03-11 13:49:40 -0500

    Update buildCoverallsDotNet.ps1

--- buildCoverallsDotNet.ps1
+++ buildCoverallsDotNet.ps1
@@ -1,4 +1,4 @@
 nuget.exe restore $env:DEPENDENCIES_DIR\coveralls.net\src\csmacnz.Coveralls.sln
-msbuild $env:DEPENDENCIES_DIR\coveralls.net\src\csmacnz.Coveralls.sln /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" /p:Configuration=Release /p:Platform="Any CPU"
+msbuild $env:DEPENDENCIES_DIR\coveralls.net\src\csmacnz.Coveralls.sln /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" /p:Configuration=Release /p:Platform="Any CPU" /v:minimal
 Add-AppveyorMessage -Category Information -Message "Installed: Coveralls.NET develop branch"
        
