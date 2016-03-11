a49e83faaf352dcc60df882b2b0ab875eb321012
afuzzyllama <afuzzyllama@users.noreply.github.com>
2016-03-11 18:08:23 -0500

    Update instrumentAssembly.ps1

--- instrumentAssembly.ps1
+++ instrumentAssembly.ps1
@@ -1,43 +1,8 @@
+.\sharedFunctions.ps1
+
 ﻿$assemblyToInstrument = $args[0]
 
-function Locate-VSVersion()
-{
-	#Find the latest version
-	$regPath = "HKLM:\SOFTWARE\Microsoft\VisualStudio"
-	if (-not (Test-Path $regPath))
-	{
-		return $null
-	}
-	
-	$keys = Get-Item $regPath | %{$_.GetSubKeyNames()} -ErrorAction SilentlyContinue
-	$version = Get-LatestVersion $keys
 
-	if ([string]::IsNullOrWhiteSpace($version))
-	{
-		return $null
-	}
-	return $version
-}
-
-function Get-LatestVersion($keys)
-{
-	[decimal]$decimalKey = $null
-	[decimal]$latestVersion = 0.0
-	[string]$latestVersionString = "00"
-	foreach ($key in $keys)
-	{
-		if([decimal]::TryParse($key, [ref]$decimalKey) -eq $true)
-		{
-			if($latestVersion -lt $decimalKey)
-			{
-				$latestVersion = $decimalKey
-				$latestVersionString = $key.Replace(".", "")
-			}
-		}
-	}
-
-	return $latestVersionString
-}
 
 $version = Locate-VSVersion
 $vsComnDir = [Environment]::GetEnvironmentVariable("VS$($version)COMNTools")
