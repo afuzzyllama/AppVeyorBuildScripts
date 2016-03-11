e9ba48a3b9cf8b594ee53980327e2172f1b57d2b
afuzzyllama <afuzzyllama@users.noreply.github.com>
2016-03-11 13:48:17 -0500

    Update instrumentAssembly.ps1

--- instrumentAssembly.ps1
+++ instrumentAssembly.ps1
@@ -1,6 +1,12 @@
 ﻿$assemblyToInstrument = $args[0]
 $vsPath = [Microsoft.Win32.Registry]::GetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\VisualStudio\14.0\", "ShellFolder", $null);
 
+
+if((Test-Path $vsPath\Team Tools\Performance Tools\vsinstr.exe") -eq $false)
+{
+    Add-AppveyorMessage -Category Error -Message "Cannot find vsinstr.exe at '$vsPath\Team Tools\Performance Tools\vsinstr.exe'"
+}
+
 Start-Process -FilePath "$vsPath\Team Tools\Performance Tools\vsinstr.exe" -ArgumentList "-coverage $assemblyToInstrument" -NoNewWindow -Wait
 
 if((Test-Path "$assemblyToInstrument.orig") -eq $false)
