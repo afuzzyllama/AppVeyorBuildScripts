39156719bc3231008ed214cd19a44e4fb834fcd1
afuzzyllama <afuzzyllama@users.noreply.github.com>
2016-03-11 15:15:24 -0500

    Update instrumentAssembly.ps1

--- instrumentAssembly.ps1
+++ instrumentAssembly.ps1
@@ -51,6 +51,7 @@ $vsinstr = "$vsComnDir\..\Team Tools\Performance Tools\vsinstr.exe"
 if((Test-Path $vsinstr) -eq $false)
 {
     Add-AppveyorMessage -Category Error -Message "Cannot find vsinstr at '$vsinstr'"
+    $host.SetShouldExit(1)
 }
 
 Start-Process -FilePath $vsinstr -ArgumentList "-coverage $assemblyToInstrument" -NoNewWindow -Wait
@@ -58,4 +59,5 @@ Start-Process -FilePath $vsinstr -ArgumentList "-coverage $assemblyToInstrument"
 if((Test-Path "$assemblyToInstrument.orig") -eq $false)
 {
     Add-AppveyorMessage -Category Error -Message "$assemblyToInstrument was not instrumented"
+    $host.SetShouldExit(1)
 }
