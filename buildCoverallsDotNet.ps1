7b98513fd16d32cbb757c815aaec68265258983f
afuzzyllama <afuzzyllama@users.noreply.github.com>
2016-03-11 10:50:47 -0500

    Update buildCoverallsDotNet.ps1

--- buildCoverallsDotNet.ps1
+++ buildCoverallsDotNet.ps1
@@ -1,1 +1,2 @@
+nuget.exe restore $env:DEPENDENCIES_DIR\coveralls.net\src\csmacnz.Coveralls.sln
 msbuild $env:DEPENDENCIES_DIR\coveralls.net\src\csmacnz.Coveralls.sln /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" /p:Configuration=Release /p:Platform="Any CPU"
