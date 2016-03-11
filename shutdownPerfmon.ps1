b5bcacb7fb12541f4b5e63a1dc78a7cbeaf0a0eb
afuzzyllama <afuzzyllama@users.noreply.github.com>
2016-03-11 18:53:34 -0500

    Update shutdownPerfmon.ps1

--- shutdownPerfmon.ps1
+++ shutdownPerfmon.ps1
@@ -6,7 +6,5 @@ $vsperfcmd = "$($vsComnDir)..\..\Team Tools\Performance Tools\vsperfcmd.exe"
 
 Start-Process -Verb runAs -FilePath $vsperfcmd -ArgumentList "/shutdown" 
 
-Get-job | Remove-Job
-
 Write-Host "Waiting 10s for vsperfmon to shutdown..."
 Start-Sleep -s 10
