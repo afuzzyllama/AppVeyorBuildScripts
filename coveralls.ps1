6e48f077679a22fc4b2bc8ff51b8b561ea028bea
afuzzyllama <afuzzyllama@users.noreply.github.com>
2016-03-11 14:13:14 -0500

    Update coveralls.ps1

--- coveralls.ps1
+++ coveralls.ps1
@@ -1,6 +1,4 @@
 ﻿$coverageXml = $args[0]
 
 $coveralls = "$env:DEPENDENCIES_DIR\coveralls.net\src\csmacnz.Coveralls\bin\Debug\csmacnz.Coveralls.exe"
-
-
 & $coveralls --exportcodecoverage -i $coverageXml -o $coverageXml.json --useRelativePaths --repoToken $env:COVERALLS_REPO_TOKEN 
